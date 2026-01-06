cc85cc2cec728e311922ec614e091a79ea5207a5
Milton Munoz <livemilton@gmail.com>
2026-01-05 20:40:37 -0500

    wheather application :sparkles: first call to API, and create an env.local to API_KEY

--- clima-react-typescript/src/App.tsx
+++ clima-react-typescript/src/App.tsx
@@ -7,6 +7,7 @@ function App() {
  
   const { fetchWeather } = useWeather() 
 
+  console.log(import.meta.env)
   return (
     <>
       <h1 className={styles.title}>Buscador de Clima</h1>
--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -1,7 +1,23 @@
+
+import axios from 'axios';
+import type { SearchType } from '../types';
+
 export default function useWeather() {
  
-    const fetchWeather =() => {
-        console.log('Consultando...')
+    const fetchWeather = async(search: SearchType) => {
+        
+        const appId = import.meta.env.VITE_API_KEY
+        try {
+            const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${search.city},${search.country}&appid=${appId}`;
+
+           //console.log(geoUrl);
+
+           const {data} = await axios.get(geoUrl);
+           console.log(data)
+
+        } catch (error) {
+            console.log(error)
+        }
     }
 
     return {
