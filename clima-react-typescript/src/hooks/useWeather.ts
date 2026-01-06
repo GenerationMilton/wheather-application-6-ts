6a7cf7880b7b0b33512e25fe11d46eafceafe0b9
Milton Munoz <livemilton@gmail.com>
2026-01-05 21:00:25 -0500

    wheather application :sparkles: type api results and assing type

--- clima-react-typescript/src/hooks/useWeather.ts
+++ clima-react-typescript/src/hooks/useWeather.ts
@@ -1,6 +1,6 @@
 
 import axios from 'axios';
-import type { SearchType } from '../types';
+import type { SearchType, Weather } from '../types';
 
 export default function useWeather() {
  
@@ -23,8 +23,9 @@ export default function useWeather() {
 
            const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${appId}`
 
-           const {data: weatherResult} = await axios(weatherUrl);
-           console.log(weatherResult);
+           const {data: weatherResult} = await axios<Weather>(weatherUrl);
+           console.log(weatherResult.name);
+           console.log(weatherResult.main.temp);
 
         } catch (error) {
             console.log(error)
